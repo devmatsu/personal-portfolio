ccc1a01812d76cf28be4bb2f52cc33b2253bbe08
Rodrigo Matagawa devmatsu <devmatsu@hotmail.com>
2025-04-14 15:48:49 -0300

    feat: add sequential fade-in & fade-out animation to TechTags component

--- src/components/TechTags.jsx
+++ src/components/TechTags.jsx
@@ -4,25 +4,74 @@ import styles from './TechTags.module.css';
 
 const TechTags = ({ tags }) => {
   const [showMore, setShowMore] = useState(false);
+  const [isAnimatingOut, setIsAnimatingOut] = useState(false);
 
-  const visibleTags = showMore ? tags : tags.slice(0, 7);
+  const visibleTags = tags.slice(0, 7);
+  const othersTags = tags.slice(7);
+
+
+  const animationDuration = othersTags.length * 50 + 100;
+  const handleToggleTags = () => {
+    if (showMore) {
+      setIsAnimatingOut(true);
+      setTimeout(() => {
+        setShowMore(false);
+        setIsAnimatingOut(false);
+      }, animationDuration);
+    } else {
+      setShowMore(true);
+    }
+  };
 
   return (
     <div>
       <div className={styles.tagsContainerWrapper}>
         <div className={styles.tagsContainer}>
-          {visibleTags.map(tag => (
-            <span className={styles.tag} key={tag}>{tag}</span>
+          {visibleTags.map((tag) => (
+            <span key={tag} className={styles.tagOpened}>
+              {tag}
+            </span>
           ))}
+
+          {othersTags.map((tag, index) => {
+            const reversedIndex = (othersTags.length - 1) - index;
+            return (
+              <span
+                key={tag}
+                className={
+                  showMore && !isAnimatingOut
+                    ? styles.tagOpened
+                    : isAnimatingOut
+                    ? styles.tagClosing
+                    : styles.tagClosed
+                }
+                style={{
+                  transitionDelay: `${
+                    showMore && !isAnimatingOut
+                      ? index * 0.05
+                      : isAnimatingOut
+                      ? reversedIndex * 0.05
+                      : 0
+                  }s`,
+                }}
+              >
+                {tag}
+              </span>
+            );
+          })}
         </div>
+
         <div className={styles.showMoreBtnWrapper}>
-          <button onClick={() => setShowMore(!showMore)} className={styles.showMoreBtn}>
-            {showMore ? 'Show less tags' : 'Show more tags'}
+          <button
+            onClick={handleToggleTags}
+            className={styles.showMoreBtn}
+          >
+            {showMore && !isAnimatingOut ? 'Show less tags' : 'Show more tags'}
           </button>
         </div>
       </div>
     </div>
-  );  
+  );
 };
 
 export default TechTags;
